99cb4d29ff1dd10bd9396f1ebf955772f6c42aee
SanchitaBassi <bassisanchita@gmail.com>
2024-01-05 21:46:29 +0530

    changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -83,9 +83,9 @@ export default function HomePage() {
                      <button id="View"> Moderate </button><br></br><br></br>
                      <button id="View"> Expertise </button><br></br><br></br>
                       </p>
-                      <div className="w-full flex justify-center pt-5 pb-5">
+                      {/* <div className="w-full flex justify-center pt-5 pb-5">
                        
-                      </div>
+                      </div> */}
                     </div>
                   </div>
                 </div>
@@ -112,10 +112,10 @@ export default function HomePage() {
                      <button id="View"> Moderate </button><br></br><br></br>
                      <button id="View"> Expertise </button><br></br><br></br>
                       </p>
-                      <div className="w-full flex justify-center pt-3 pb-3">
+                      {/* <div className="w-full flex justify-center pt-3 pb-3">
                         <p className="text-black-400 text-sm text-center font-Times New Roman ">
                         Web development is the work involved in developing a website for the Internet .</p>
-                      </div>
+                      </div> */}
                     </div>
                   </div>
                 </div>
@@ -142,9 +142,7 @@ export default function HomePage() {
                      <button id="View"> Moderate </button><br></br><br></br>
                      <button id="View"> Expertise </button><br></br><br></br>
                       </p>
-                      <div className="w-full flex justify-center pt-5 pb-5">
-                        
-                      </div>
+                    
                     </div>
                   </div>
                 </div>
@@ -171,9 +169,9 @@ export default function HomePage() {
                      <button id="View"> Moderate </button><br></br><br></br>
                      <button id="View"> Expertise </button><br></br><br></br>
                       </p>
-                      <div className="w-full flex justify-center pt-5 pb-5">
+                      {/* <div className="w-full flex justify-center pt-5 pb-5">
                         
-                      </div>
+                      </div> */}
                     </div>
                   </div>
                 </div>
@@ -200,9 +198,7 @@ export default function HomePage() {
                      <button id="View"> Moderate </button><br></br><br></br>
                      <button id="View"> Expertise </button><br></br><br></br>
                       </p>
-                      <div className="w-full flex justify-center pt-5 pb-5">
-                        
-                      </div>
+                      
                     </div>
                   </div>
                 </div>
@@ -211,8 +207,8 @@ export default function HomePage() {
                     <div className="absolute -mt-20 w-full flex justify-center">
                       <div className="h-32 w-32">
                         <img
-                          src="https://cdn.tuk.dev/assets/photo-1530577197743-7adf14294584.jfif"
-                          alt="#teammember6"
+                          src="images/data-analysis.png"
+                          alt="#data-analysis"
                           className="rounded-full object-cover h-full w-full shadow-md"
                         />
                       </div>
@@ -222,80 +218,24 @@ export default function HomePage() {
                         Data Analysis
                       </div>
                       <p className="text-gray-800 text-sm text-center">
-                        
+                        data
                       </p>
                       <p className="text-center text-gray-600 text-base pt-3 font-normal">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget sapien sit amet nunc accumsan consectetur. Curabitur euismod convallis neque, vitae placerat turpis interdum eget. Quisque auctor metus eu nulla consectetur, sed eleifend turpis fermentum. 
-                      Ut euismod lorem vitae justo ullamcorper, quis sagittis nunc convallis. Integer sed elit id libero suscipit efficitur.
+                      <button id="View"> Basics </button> <br></br> <br></br>
+                     <button id="View"> Moderate </button><br></br><br></br>
+                     <button id="View"> Expertise </button><br></br><br></br>
                       </p>
-                      <div className="w-full flex justify-center pt-5 pb-5">
-                        <a href="#" className="mx-5">
-                          <div>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width={24}
-                              height={24}
-                              viewBox="0 0 24 24"
-                              fill="none"
-                              stroke="#718096"
-                              strokeWidth="1.5"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              className="feather feather-github"
-                            >
-                              <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
-                            </svg>
-                          </div>
-                        </a>
-                        <a href="#" className="mx-5">
-                          <div>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width={24}
-                              height={24}
-                              viewBox="0 0 24 24"
-                              fill="none"
-                              stroke="#718096"
-                              strokeWidth="1.5"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              className="feather feather-twitter"
-                            >
-                              <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
-                            </svg>
-                          </div>
-                        </a>
-                        <a href="#" className="mx-5">
-                          <div>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width={24}
-                              height={24}
-                              viewBox="0 0 24 24"
-                              fill="none"
-                              stroke="#718096"
-                              strokeWidth="1.5"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              className="feather feather-instagram"
-                            >
-                              <rect
-                                x={2}
-                                y={2}
-                                width={20}
-                                height={20}
-                                rx={5}
-                                ry={5}
-                              />
-                              <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
-                              <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" />
-                            </svg>
-                          </div>
-                        </a>
-                      </div>
+                     
                     </div>
                   </div>
+                  
                 </div>
+                
+              </div>
+              <div className="display-flex justify-center">
+              <p className="text-gray-800 text-sm text-center ">
+              <button className=" bg-#718e94-800 px-6 py-3 text-white rounded-3xl w-40 " id="Courses"> View More Courses</button>
+              </p>
               </div>
             </div>
           </div>
@@ -303,7 +243,7 @@ export default function HomePage() {
 
         <div className="2xl:container 2xl:mx-auto">
           <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 lg:gap-24 md:gap-10 gap-12 lg:px-20 md:py-12 md:px-6 py-9 px-4">
-            {/* Free Shipping Grid Card */}
+            {/* Free Shipping Grid Card */} 
             <div className="flex space-x-4">
               <svg
                 width="40"
