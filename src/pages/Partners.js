a0ecca51921f62ef5d84cdf0a2f8a695af2ed099
anirudh094 <anirudhgarg094@gmail.com>
2024-01-25 02:12:33 +0530

    Partners

--- src/pages/Partners.js
+++ src/pages/Partners.js
@@ -15,19 +15,22 @@ function Partners() {
             <div className="heading main-color">Our Partners</div>
             <div className="carousel partners-carousel" data-flickity='{"groupCells": 1,"autoPlay": 900,"pauseAutoPlayOnHover": false, "wrapAround": true,"prevNextButtons": false, "pageDots": false}'>
                 <div className="carousel-cell partners-cell">
-                    <img src="images/4.png" alt="partners"></img>
+                    <img src="https://etimg.etb2bimg.com/photo/89939048.cms" alt="partners"></img>
                 </div>
                 <div className="carousel-cell partners-cell">
-                    <img src="images/4.png" alt="partners"></img>
+                    <img src="https://securein.com/wp-content/uploads/2023/07/SECURE_INNOVATION_A2M_PASSIVE_LOGO_POS_RGB-01.png" alt="partners"></img>
                 </div>
                 <div className="carousel-cell partners-cell">
-                    <img src="images/4.png" alt="partners"></img>
+                    <img src="https://media.licdn.com/dms/image/C4E16AQH_h_2c6LTXkQ/profile-displaybackgroundimage-shrink_200_800/0/1625833149879?e=2147483647&v=beta&t=WCszSLKwWpmzJeP4dsOUxLQJ1k7SkOCGK5rr3CiTvc4" alt="partners"></img>
                 </div>
                 <div className="carousel-cell partners-cell">
-                    <img src="images/4.png" alt="partners"></img>
+                    <img src="https://www.penthara.com/wp-content/uploads/2023/01/PentharaBeGood.webp" alt="partners"></img>
                 </div>
                 <div className="carousel-cell partners-cell">
-                    <img src="images/4.png" alt="partners"></img>
+                    <img src="https://upload.wikimedia.org/wikipedia/en/d/d8/MTV_Hustle_logo.webp" alt="partners"></img>
+                </div>
+                <div className="carousel-cell partners-cell">
+                    <img src="https://mms.businesswire.com/media/20230809463056/en/1147549/22/AVM_logo_highres.jpg" alt="partners"></img>
                 </div>
             </div>
             
